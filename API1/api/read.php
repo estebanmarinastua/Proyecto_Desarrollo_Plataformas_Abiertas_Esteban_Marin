d73429c22fcf858ea5184c133d3ee9d6fecb7ec2
estebanmarinastua <emarinastua@gmail.com>
2024-12-18 16:40:48 -0600

    Update read.php

--- API1/api/read.php
+++ API1/api/read.php
@@ -1,1 +1,13 @@
+<?php
+require_once '../config/database.php';
+require_once '../controllers/PrendaController.php';
 
+$database = new Database();
+$db = $database->getConnection();
+
+$controller = new PrendaController($db);
+$records = $controller->read();
+
+header('Content-Type: application/json');
+echo json_encode($records);
+?>
