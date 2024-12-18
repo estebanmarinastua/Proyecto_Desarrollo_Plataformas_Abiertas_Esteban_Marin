0b777f3588300749ea7ee1532e6960f01cf44584
estebanmarinastua <emarinastua@gmail.com>
2024-12-18 16:32:58 -0600

    Update MarcaController.php

--- API1/controllers/MarcaController.php
+++ API1/controllers/MarcaController.php
@@ -1,1 +1,23 @@
+<?php
+require_once __DIR__ . '/../models/Marca.php';
 
+class MarcaController {
+    private $marca;
+
+    public function __construct($db) {
+        $this->marca = new Marca($db);
+    }
+
+    public function getMarcas() {
+        return json_encode($this->marca->getAll());
+    }
+
+    public function createMarca($nombre) {
+        if ($this->marca->create($nombre)) {
+            return json_encode(['message' => 'Marca creada con éxito']);
+        } else {
+            return json_encode(['message' => 'Error al crear la marca']);
+        }
+    }
+}
+?>
