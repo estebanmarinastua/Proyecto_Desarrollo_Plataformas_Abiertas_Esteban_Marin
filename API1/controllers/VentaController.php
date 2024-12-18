847c7ec6e8eeecee0b2b62001391eeb84266abc2
estebanmarinastua <emarinastua@gmail.com>
2024-12-18 16:34:10 -0600

    Update VentaController.php

--- API1/controllers/VentaController.php
+++ API1/controllers/VentaController.php
@@ -1,1 +1,23 @@
+<?php
+require_once __DIR__ . '/../models/Venta.php';
 
+class VentaController {
+    private $venta;
+
+    public function __construct($db) {
+        $this->venta = new Venta($db);
+    }
+
+    public function getVentas() {
+        return json_encode($this->venta->getAll());
+    }
+
+    public function createVenta($id_prenda, $fecha_venta, $cantidad) {
+        if ($this->venta->create($id_prenda, $fecha_venta, $cantidad)) {
+            return json_encode(['message' => 'Venta creada con éxito']);
+        } else {
+            return json_encode(['message' => 'Error al crear la venta']);
+        }
+    }
+}
+?>
